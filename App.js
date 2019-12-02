ad340a45d479c3abf8439c1d903d537156bd6169
Abdullah Sami <abdullahsamisherazi@gmail.com>
2019-12-02 20:45:04 +0500

    code alignment done

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
-import React,{Component} from 'react';
-import {createStackNavigator,createAppContainer} from 'react-navigation';
-import Store from './src/Store';  
-import CreateCart from './src/CreateCart';  
+import React, { Component } from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Store from "./src/Store";
+import CreateCart from "./src/CreateCart";
 
-const AppNavigator = createStackNavigator(  
-    {
-     Store:{      
-       screen: Store,
-        navigationOptions: () => ({
-           header:null
-        })
+const AppNavigator = createStackNavigator(
+  {
+    Store: {
+      screen: Store,
+      navigationOptions: () => ({
+        header: null
+      })
     },
-    CreateCart:{      
+    CreateCart: {
       screen: CreateCart,
-       navigationOptions: () => ({
-          header:null
-       })
-   },
-  },
-    {
-      initialRouteName: 'Store'
+      navigationOptions: () => ({
+        header: null
+      })
     }
-  );
+  },
+  {
+    initialRouteName: "Store"
+  }
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
-export default class App extends Component {  
+export default class App extends Component {
   render() {
     return <AppContainer />;
   }
